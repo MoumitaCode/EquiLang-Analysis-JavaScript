b7e1a432e62cedaae78ca9a5bdd1a48ead235060
Moumita Ray <moumitaray@Moumitas-MacBook-Air.local>
2025-02-08 12:09:12 -0500

    Second Commit

--- equi_lang.js
+++ equi_lang.js
@@ -1,1 +1,7 @@
-const dfd = require("danfojs-node"); // Import danfo.js for data handling
+// Import danfo.js for data handling
+const dfd = require("danfojs-node"); 
+const danfo = require('danfojs-node');
+const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+
+console.log("Welcome to the EquiLang Analysis Tool!");
+console.log("This tool will help you explore data related to English Language Learners (ELL).");
